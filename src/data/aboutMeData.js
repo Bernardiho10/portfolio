79ed21cf66acd6533ce24ea4a2635e46238ca49b
Bernard Oko <bernardarikuoko@gmail.com>
2025-06-05 08:24:18 +0100

    Edited my Bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,18 +2,23 @@ export const aboutMeData = [
   {
     id: 1,
     bio: `
-		Dedicated software Developer specializing in MERN(MongoDB, Express, React, Nodejs) stack development. My professional journey at Efiko freelance, Body Motions Physio and Books Embassy, I've fine-tuned my technical skills and cultivated a profound passion for AI(Artifical Intelligence) and Information Technology. From enhancing digital experiences for patients to developing scalable web infrastructures, I've gained diverse insights and experiences.
+    I am a self-taught software engineer with over 5 years of hands-on experience building scalable web applications and collaborating with diverse businesses to enhance their online presence. My journey into tech began through platforms like Treehouse, Andela, and Microsoft Learn, where I developed a strong foundation in software development and computer science principles. Specializing in full-stack development, I focus heavily on JavaScript frameworks such as React and Node.js to deliver high-quality, user-centered solutions.
 
 		`,
   },
   {
     id: 2,
     bio: `
-		My Technical expertise in the MERN stack has been instrumental in creating robust, scalable web applications that meet the needs of users and stakeholders alike.
-		`,
+    As a freelancer, I’ve partnered with companies across industries to design, build, and maintain web platforms that improve customer engagement and drive business growth. My experience spans working with RESTful APIs, microservices architecture, and cloud platforms like AWS and Azure. I'm also proficient in both SQL and NoSQL databases, ensuring reliable data performance and integrity.
+    I am a lifelong learner, constantly seeking to expand my knowledge and stay updated with the latest industry trends. My goal is to leverage my skills to keep building impactful solutions that drive business success and improve user experiences.
+
+    `,
   },
   {
     id: 3,
-    bio: `I hold a Microsoft Azure AI Fundamentals and Azure AI Engineer Associate Certificates. I am currently pursuing further certifications in Microsoft AI Solutions Expert, demonstrating my commitment to continuous learning. Beyond my professional pursuits, I believe in maintaining a balanced lifestyle. I enjoys exercising my body to stay physically fit and reading to exercise my mind, I constantly seek new knowledge and perspectives.`
+    bio: `
+    I hold Microsoft Certified: Azure AI Fundamentals and Azure AI Engineer Associate certifications, and I am currently pursuing the Microsoft Certified: AI Solutions Expert certification—demonstrating my commitment to continuous learning and growth in the field of Artificial Intelligence. 
+    Beyond my professional pursuits, I believe in maintaining a balanced lifestyle. I enjoy exercising to stay physically fit and reading to keep my mind sharp. I'm constantly seeking new knowledge and perspectives, both in technology and in life.
+    `,
   }
 ];

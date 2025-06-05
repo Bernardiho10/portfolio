b4838c54ad0deac0446c68ee99693a00c62f9579
Bernard Oko <bernardarikuoko@gmail.com>
2025-06-05 06:55:30 +0100

    Zoho mail Verification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import AppFooter from "./components/shared/AppFooter";
 import AppHeader from "./components/shared/AppHeader";
+
 import "./css/App.css";
 import UseScrollToTop from "./hooks/useScrollToTop";
 import PageNotFound from "./pages/PageNotFound";
@@ -14,24 +15,45 @@ const Home = lazy(() => import("./pages/Home"));
 const Projects = lazy(() => import("./pages/Projects"));
 const ProjectSingle = lazy(() => import("./pages/ProjectSingle.jsx"));
 
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "projects",
+    element: <Projects />,
+  },
+  {
+    path: "/single-project/:projectId",
+    element: <ProjectSingle />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+];
+
 function App() {
   return (
     <AnimatePresence>
-      <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+      <div className="bg-secondary-light dark:bg-primary-dark transition duration-300">
         <Router>
           <ScrollToTop />
           <AppHeader />
           <Suspense fallback={""}>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="projects" element={<Projects />} />
-              <Route
-                path="/single-project/:projectId"
-                element={<ProjectSingle />}
-              />
-
-              <Route path="contact" element={<Contact />} />
-              <Route path="*" element={<PageNotFound />} />
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
             </Routes>
           </Suspense>
           <AppFooter />

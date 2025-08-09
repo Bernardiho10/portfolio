d55b6127e0d752f8431a4fadeb6bf01a1a3eb44b
Bernard Oko <bernardarikuoko@gmail.com>
2025-08-09 15:21:43 +0100

    Cleaned up bio and updated projects date

--- src/components/about/AboutCounter.jsx
+++ src/components/about/AboutCounter.jsx
@@ -2,7 +2,7 @@ import { useCountUp } from "react-countup";
 import CounterItem from "./CounterItem";
 
 const AboutCounter = () => {
-  useCountUp({ ref: "experienceCounter", end: 4, duration: 2 });
+  useCountUp({ ref: "experienceCounter", end: 3, duration: 2 });
   useCountUp({ ref: "githubStarsCounter", end: 5, duration: 2 });
   useCountUp({ ref: "feedbackCounter", end: 80, duration: 2 });
   useCountUp({ ref: "projectsCounter", end: 75, duration: 2 });
